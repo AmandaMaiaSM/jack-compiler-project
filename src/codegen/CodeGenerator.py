39f3703fd9eeece071908504998a7010a2f3a3f2
Marcelo Garces <BRANCO.MARCOS@DISCENTE.UFMA.BR>
2026-05-23 18:59:34 -0300

    aplicando metodos de declaracao, corpo de subrotinas, variaeis locais

--- src/codegen/CodeGenerator.py
+++ src/codegen/CodeGenerator.py
@@ -56,3 +56,58 @@ class CodeGenerator:
             name = self._consume('identifier').value
             self._sym.define(name, type_, kind)
         self._consume('symbol', ';')
+
+
+    def _compile_subroutine(self):
+        self._sub_type = self._consume('keyword').value   # constructor|function|method
+        self._consume()                                    # return type
+        sub_name = self._consume('identifier').value
+        self._consume('symbol', '(')
+        self._sym.start_subroutine(self._sub_type)
+        self._compile_parameter_list()
+        self._consume('symbol', ')')
+        self._compile_subroutine_body(sub_name)
+
+    def _compile_parameter_list(self):
+        if self._match('symbol', ')'):
+            return
+        type_ = self._consume_type()
+        name = self._consume('identifier').value
+        self._sym.define(name, type_, 'arg')
+        while self._match('symbol', ','):
+            self._consume('symbol', ',')
+            type_ = self._consume_type()
+            name = self._consume('identifier').value
+            self._sym.define(name, type_, 'arg')
+
+    def _compile_subroutine_body(self, sub_name):
+        self._consume('symbol', '{')
+        # Collect var declarations BEFORE emitting function header (need nLocals)
+        while self._match('keyword', 'var'):
+            self._compile_var_decl()
+        n_locals = self._sym.var_count('local')
+        self._vm.write_function(f'{self._class_name}.{sub_name}', n_locals)
+        self._emit_subroutine_prologue()
+        self._compile_statements()
+        self._consume('symbol', '}')
+
+    def _emit_subroutine_prologue(self):
+        if self._sub_type == 'constructor':
+            n_fields = self._sym.var_count('field')
+            self._vm.write_push('constant', n_fields)
+            self._vm.write_call('Memory.alloc', 1)
+            self._vm.write_pop('pointer', 0)
+        elif self._sub_type == 'method':
+            self._vm.write_push('argument', 0)
+            self._vm.write_pop('pointer', 0)
+
+    def _compile_var_decl(self):
+        self._consume('keyword', 'var')
+        type_ = self._consume_type()
+        name = self._consume('identifier').value
+        self._sym.define(name, type_, 'local')
+        while self._match('symbol', ','):
+            self._consume('symbol', ',')
+            name = self._consume('identifier').value
+            self._sym.define(name, type_, 'local')
+        self._consume('symbol', ';')
