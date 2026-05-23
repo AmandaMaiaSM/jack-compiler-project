faedeafa7b8f5ad1295a567159b252c408dc7769
AmandaMaiaSm <amandamaiadev@gmail.com>
2026-05-23 18:52:38 -0300

    inicializando escopo de subrotina, variaveis

--- src/codegen/SymbolTable.py
+++ src/codegen/SymbolTable.py
@@ -11,3 +11,15 @@ class SymbolTable:
         self._sub_table = {}
         self._counts = {'static': 0, 'field': 0, 'arg': 0, 'local': 0}
 
+    def start_subroutine(self, subroutine_type='function'):
+        self._sub_table = {}
+        self._counts['arg'] = 1 if subroutine_type == 'method' else 0
+        self._counts['local'] = 0
+
+    def define(self, name, type_, kind):
+        entry = {'type': type_, 'kind': kind, 'index': self._counts[kind]}
+        self._counts[kind] += 1
+        if kind in ('static', 'field'):
+            self._class_table[name] = entry
+        else:
+            self._sub_table[name] = entry
